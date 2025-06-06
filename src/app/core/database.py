8ec4b4b75e9c4fe825fbe1f2e7a6f2c0526daea0
Palash Pramanick <palash14@gmail.com>
2025-06-06 16:54:16 +0530

    Update database.py

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,29 +1,42 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# File: src/app/core/database.py
+"""
+This module contains the database connection and session management code.
+It uses SQLAlchemy as the ORM and asynchronous connections to a PostgreSQL database.
+"""
+
+from __future__ import annotations
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base 
 from src.app.core.config import settings
 
-# Create the SQLAlchemy engine using the database URI from settings
-engine = create_engine(str(settings.DATABASE_URI))
-
-# Create a configured "SessionLocal" class
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a base class for declarative class definitions
+# Base class for declarative models
 Base = declarative_base()
 
-
-def get_db():
+# Create the SQLAlchemy engine using the database URI from settings
+engine = create_async_engine(
+    url=str(settings.DATABASE_URI),
+    # Log SQL statements for debugging
+    echo=True,
+    # Pool size
+    pool_size=5,
+    # The size of the pool to be maintained, or None for unbounded pool size
+    max_overflow=10,
+)
+# Dependency for getting a DB session
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """
-    Dependency that provides a SQLAlchemy database session.
+    Provides an async SQLAlchemy session for dependency injection.
 
     Yields:
-        Session: A SQLAlchemy session object.
-
-    Ensures that the session is properly closed after use.
+        AsyncSession: An instance of SQLAlchemy async session.
     """
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    # Async session factory
+    async_session = async_sessionmaker(
+        bind=engine,
+        class_=AsyncSession,  # Type of session to create
+        autocommit=False,  # Don't commit automatically
+        expire_on_commit=False,
+    )
+    async with async_session() as session:
+        yield session
